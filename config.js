17e89c87f230ed5cb8e45850c9bd018cb21b49a6
Arslan-MD <157588592+ArslanMDofficial@users.noreply.github.com>
2025-03-28 07:28:00 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+SESSION_ID: process.env.SESSION_ID || "Arslan-XD~Bk4xgSjY#NvfA7_AkxrFGZ7bkQYR-yZKOXPGPN2o70C-SZfUuEkg",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
